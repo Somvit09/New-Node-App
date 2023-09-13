1c1475996a7296948f4648db3f6dfdd1e06cce05
Somvit09 <developer.09.sam@gmail.com>
2023-09-14 02:04:57 +0530

    updated the routes and controllers

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors'); //Cross-Origin Resource Sharing (CORS) middleware
 const multer = require('multer'); // middleware for handling file uploads
+
 const app = express();
-require('dotenv').config(); // load varialbles from .env
+
+// load varialbles from .env
+require('dotenv').config(); 
 const port = process.env.PORT;
 const database = process.env.DATABASE_NAME;
 // for otp
@@ -12,6 +15,8 @@ const twilioAccountSid = process.env.YOUR_TWILIO_ACCOUNT_SID;
 const twilioAuthToken = process.env.YOUR_TWILIO_AUTH_TOKEN;
 const twilioPhoneNumber = process.env.YOUR_TWILIO_PHONE_NUMBER;
 
+// router configuration
+const apparelRoutes = require("./routes/crud")
 
 // initializing the Twilio client
 const client = twilio(twilioAccountSid, twilioAuthToken);
@@ -40,73 +45,10 @@ const Customer = require('./models/customer_model');
 const Merchant = require('./models/merchant_model');
 const OTP = require('./models/otpModel');
 
-// Create an Apparel
 
-app.post('/apparel', async (req, res) => {
-    try{
-        const newApparel = new Apparel(req.body);
-        const savedApparel = await newApparel.save();
-        res.status(201).json(savedApparel);
-    } catch (err) {
-        res.status(400).json({'error': err.message});
-    }
-});
+// routers for crud in apparel
 
-// Get all Apparel
-
-app.get('/apparel', async(req, res) => {
-    try{
-        const allApparel = await Apparel.find();
-        res.json(allApparel);
-    } catch (err) {
-        res.status(500).json({error: err.message});
-    }
-});
-
-// Get a specific apparel by ID
-
-app.get('/apparel/:id', async(req, res) => {
-    try{
-        const apparel = await Apparel.findOne({apparelID: req.params.id}); // using findOne function to get the object associated with apparel id 
-        if (!apparel){
-            return res.status(404).json({error: 'Apparel Not found.'});
-        }
-        res.json(apparel);
-    } catch (err) {
-        res.status(500).json({error: err.message});
-    }
-});
-
-// Update an apparel by apparelID
-
-app.put('/apparel/:id', async(req, res) => {
-    try{
-        const updatedApparel = await Apparel.findOneAndUpdate(
-            {apparelID: req.params.id},
-            req.body,
-            {new : true});
-        if (!updatedApparel) {
-            return res.status(404).json({error: 'Apparel not found'});
-        } 
-        res.json(updatedApparel);
-    } catch (err) {
-        res.status(500).json({error: err.message});
-    }
-});
-
-// Delete an apparel 
-
-app.delete('/apparel/:id', async(req, res) => {
-    try{
-        deleteApparel = await Apparel.findOneAndDelete({apparelID: req.params.id});
-        if (!deleteApparel){
-            return res.status(404).json({error: "Apparel not gound."});
-        }
-        res.json(deleteApparel);
-    } catch (err) {
-        res.status(500).json({error: err.message});
-    }
-});
+app.use('/apparel', apparelRoutes)
 
 
 // Helper function to generate a random OTP
